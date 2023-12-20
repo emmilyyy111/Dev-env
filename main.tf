a5e089e0ec3ca1cfd13e50d5ef5a59f004a664d9
emilymorgenbesser <morgenbesseremily@gmail.com>
2023-12-20 11:35:10 -0500

    adds iam role

--- main.tf
+++ main.tf
@@ -6,7 +6,49 @@ terraform {
             source = "hashicorp/aws"
             version = ">= 3.24"
         }
-
     }
-
 }
+
+variable "aws_region" {
+    type = string
+    default = "us-east-1"
+}
+
+provider "aws" {
+    region = var.aws_region
+}
+
+data "archive_file" "myzip" {
+    type = "zip"
+    source_file = "main.py"
+    output_path = "main.zip"
+}
+
+resource "aws_lambda_function" "mypython_lambda" {
+    filename = "main.zip"
+    function_name = "mypython_lambda_test"
+    role = aws_iam_role.mypython_lambda_role.arn
+    handler = "lambda_handler"
+    runtime = "python2.7"
+}
+
+resource "aws_iam_role" "mypython_lambda_role" {
+    name = "mypython_role"
+    
+    assume_role_policy = <<EOF
+{
+      "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "sts:AssumeRole",
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                },
+                "Effect": "Allow",
+                "Sid": ""
+            }
+        ]
+    }
+    EOF
+}
+
